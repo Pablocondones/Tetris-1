5e59fc8c69c59a6635fc9a6b933eae7dccda225a
Pablo-branch <Abolfazl@DESKTOP-OA2RD6G>
2026-03-24 12:08:56 +0100

    timer funktion (beginning)  addad

--- script.js
+++ script.js
@@ -3,9 +3,13 @@ const startBtn = document.getElementById("start-btn");
 const gameContainer = document.getElementById("game-container");
 const menu = document.getElementById("menu");
 
+let totalElapsedTime = 0;
+let lastTime = 0;
 let gameRunning = false;    
 
 function startGame() {
+    totalElapsedTime = 0;
+    lastTime = performance.now();
     gameRunning = true;
     
     menu.style.display = "none";
@@ -17,3 +21,22 @@ function startGame() {
 
 startBtn.addEventListener("click", startGame);
 
+function update(time = 0) {
+    if (gameRunning) {
+        const deltaTime = time - lastTime;
+        lastTime = time;
+
+        totalElapsedTime += deltaTime;
+        const totalSeconds = Math.floor(totalElapsedTime / 1000);
+        
+        const mins = Math.floor(totalSeconds / 60).toString().padStart(2, '0');
+        const secs = (totalSeconds % 60).toString().padStart(2, '0');
+        
+        const timerElement = document.getElementById('timer');
+        if (timerElement) {
+            timerElement.innerText = `${mins}:${secs}`;
+        }
+
+        requestAnimationFrame(update);
+    }
+}
