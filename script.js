ac6a5e63d679abdca2f6b2b518293e0974770ce7
Melvin <omanagerad@DESKTOP-AL0KS3U>
2026-04-07 13:12:56 +0200

    twst

--- script.js
+++ script.js
@@ -67,12 +67,3 @@ function update(time = 0) {
     }
 }
 });
-function createPiece(type){
-    if (type === 'I') return [[0,1,0,0]],[[0,1,0,0]],[[0,1,0,0]],[[0,1,0,0]];
-    if (type === 'L') return [[0,2,0]],[[0,2,0]],[[0,2,2]];
-    if (type === 'J') return [[0,3,0]],[[0,3,0]],[[3,3,0]];
-    if (type === 'O') return [[4,4]],[[4,4]];
-    if (type === 'Z') return [[5,5,0]],[[0,5,5]],[[0,0,0]];
-    if (type === 'S') return [[0,6,6]],[[6,6,0]],[[0,0,0]];
-    if (type === 'T') return [[0,7,0]],[[7,7,7]],[[0,0,0]];
-}
